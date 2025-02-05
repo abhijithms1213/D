b3731efee6ffb076603f09a289cc3daaa3acefe7
Abhijith <abhijithms1213@gmail.com>
2025-02-05 19:56:11 +0530

    Revert "solving"
    This reverts commit de5b59ca2a5e715be92a6459b5bf4e50b20dc095.

--- salhurry/lib/main.dart
+++ salhurry/lib/main.dart
@@ -8,7 +8,12 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: const FirebaseOptions(
-    
+      apiKey: "AIzaSyBB70FcflP1V7etwZFZWvxkmfldlhHMOtE",
+      authDomain: "salhurrie.firebaseapp.com",
+      projectId: "salhurrie",
+      storageBucket: "salhurrie.appspot.com",
+      messagingSenderId: "141286281817",
+      appId: "1:141286281817:android:e68e0c4bd563d3d3b1d748",
     ),
   );
   runApp(const MyApp());
